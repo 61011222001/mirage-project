4ac6589c6123c270e35bc065c958b0014fa6c7b3
gold <61011222001@msu.ac.th>
2021-04-11 23:26:22 +0700

    update-rewrite-b

--- charactors/main_charactor.js
+++ charactors/main_charactor.js
@@ -3,6 +3,7 @@ class MainCharactor extends Charactor {
         super((id == null) ? "main_charactor@" + Math.floor(Math.random()*1000) : id, 
             (className == null) ? "main_charactor" : className);
         
+        this.setSpeed(4.0);
         this.setSize(50, 50);
         this.setPosition(0, 0);
 
--- scenes/main_scene.js
+++ scenes/main_scene.js
@@ -1,21 +1,30 @@
 class MainScene extends Scene {
     mainCharactor;
+    mainCharactorLabel;
 
     constructor (id="main_scene", className="scene") {
         super(id, className);
 
         this.mainCharactor = new MainCharactor();
+        this.mainCharactorLabel = new Label();
+
         this.mainCharactor.setPosition(500, 100);
+
+        this.mainCharactorLabel.setOpacity(0.5);
+        this.mainCharactorLabel.setFontSize(12);
+        this.mainCharactorLabel.setSize(100, 15);
         
         this.addChild(this.mainCharactor);
+        this.addChild(this.mainCharactorLabel);
     }
 
     setup (then=null) {
         super.setup(then);
 
         this.mainCharactor.setup((obj) => {
-            obj.loop(() => {
-                // code..
+            obj.loop((obj) => {
+                this.mainCharactorLabel.setText(obj.x + ", " + obj.y);
+                this.mainCharactorLabel.setPosition(obj.x, obj.y-15);
             });
         });
     }
